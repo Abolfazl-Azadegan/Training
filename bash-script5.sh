6a6cae4a10b05162e29579ca9a1839ecbe44d2e2
Abolfazl Azadegan <abolfazl.azadegan@yahoo.com>
2024-03-23 02:10:33 +0330

    added the script about the IF conditions

---
+++ bash-script5.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+
+#This is a script which will focus on IF conditions
+#
+#
+#This is the structure of the IF statement
+#
+#if CONDITION then
+   # Code to execute if the condition is true
+#fi
+#
+#
+#here are the examples
+#
+#
+
+
+
+
