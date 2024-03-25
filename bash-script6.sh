b60fe30c857c7ceed85b62338a347d3edca5aea3
Abolfazl Azadegan <abolfazl.azadegan@yahoo.com>
2024-03-26 01:32:09 +0330

    added new comment for file bash-script6.sh

---
+++ bash-script6.sh
@@ -0,0 +1,14 @@
+docker --version
+
+echo "Hello World"
+
+java -version
+
+
+#for running the script we should note the following:
+
+#1. we don't have sheebang at the beginning of this script, but we have added the execute permission to this file so it will run correct with the "./scriptname" command
+
+#2. if we didn't add the execute permission to this file we should use "bash scriptname" command to run the script.
+
+
